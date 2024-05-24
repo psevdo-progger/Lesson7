5de794211d5ada858a0a2415588015811d28b6e4
Daria <Usatykdasha33@gmail.com>
2024-05-24 03:58:51 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lesson7"
+include(":app")
+include(":httpurlconnection")
+include(":firebaseauth")
+include(":timeservice")
+include(":timeservice2")
+include(":firebaseauth2")
+include(":timeservice3")
---
+++ timeservice3/src/main/java/com/mirea/usatyukds/timeservice3/MainActivity.java
@@ -0,0 +1,57 @@
+package com.mirea.usatyukds.timeservice3;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mirea.usatyukds.timeservice3.databinding.ActivityMainBinding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private final String host = "time.nist.gov"; // или time-a.nist.gov
+    private final int port = 13;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetTimeTask timeTask = new GetTimeTask();
+                timeTask.execute();
+            }
+        });
+    }
+    private class GetTimeTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            String timeResult = "";
+            try {
+                Socket socket = new Socket(host, port);
+                BufferedReader reader = SocketUtils.getReader(socket);
+                reader.readLine(); // игнорируем первую строку
+                timeResult = reader.readLine(); // считываем вторую строку
+                Log.d(TAG,timeResult);
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return timeResult;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            binding.textView.setText(result);
+        }
+    }
+}
